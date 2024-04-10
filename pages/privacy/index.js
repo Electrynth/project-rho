65bf36d2e949c437d26e3706be3bfd6c6a563872
Nicholas Brown <nicholas.brown@jpl.nasa.gov>
2024-04-10 16:50:22 -0700

    Fix linter issues

--- pages/privacy/index.js
+++ pages/privacy/index.js
@@ -8,7 +8,7 @@ export default function Privacy() {
             <p>Effective date: April 10, 2024</p>
 
 
-            <p>HoloFoundry ("us", "we", or "our") operates the https://HoloFoundry.net/ website (hereinafter referred to as the "Service").</p>
+            <p>HoloFoundry (&#34us&#34, &#34we&#34, or &#34our&#34) operates the https://HoloFoundry.net/ website (hereinafter referred to as the &#34Service&#34).</p>
 
             <p>This page informs you of our policies regarding the collection, use, and disclosure of personal data when you use our Service and the choices you have associated with that data. The Privacy Policy for HoloFoundry has been created with the help of <a href="https://www.termsfeed.com/privacy-policy-generator/">Privacy Policy Generator</a>.</p>
 
@@ -39,7 +39,7 @@ export default function Privacy() {
             <h3>Types of Data Collected</h3>
 
             <h4>Personal Data</h4>
-            <p>While using our Service, we may ask you to provide us with certain personally identifiable information that can be used to contact or identify you ("Personal Data"). Personally identifiable information may include, but is not limited to:</p>
+            <p>While using our Service, we may ask you to provide us with certain personally identifiable information that can be used to contact or identify you (&#34Personal Data&#34). Personally identifiable information may include, but is not limited to:</p>
 
             <ul>
             <li>Email address</li><li>First name and last name</li><li>Cookies and Usage Data</li>
@@ -47,7 +47,7 @@ export default function Privacy() {
 
             <h4>Usage Data</h4>
 
-            <p>We may also collect information how the Service is accessed and used ("Usage Data"). This Usage Data may include information such as your computer's Internet Protocol address (e.g. IP address), browser type, browser version, the pages of our Service that you visit, the time and date of your visit, the time spent on those pages, unique device identifiers and other diagnostic data.</p>
+            <p>We may also collect information how the Service is accessed and used (&#34Usage Data&#34). This Usage Data may include information such as your computer&#39s Internet Protocol address (e.g. IP address), browser type, browser version, the pages of our Service that you visit, the time and date of your visit, the time spent on those pages, unique device identifiers and other diagnostic data.</p>
 
             <h4>Tracking & Cookies Data</h4>
             <p>We use cookies and similar tracking technologies to track the activity on our Service and we hold certain information.</p>
@@ -97,25 +97,25 @@ export default function Privacy() {
             <p>The security of your data is important to us but remember that no method of transmission over the Internet or method of electronic storage is 100% secure. While we strive to use commercially acceptable means to protect your Personal Data, we cannot guarantee its absolute security.</p>
 
             <h2>Service Providers</h2>
-            <p>We may employ third party companies and individuals to facilitate our Service ("Service Providers"), to provide the Service on our behalf, to perform Service-related services or to assist us in analyzing how our Service is used.</p>
+            <p>We may employ third party companies and individuals to facilitate our Service (&#34Service Providers&#34), to provide the Service on our behalf, to perform Service-related services or to assist us in analyzing how our Service is used.</p>
             <p>These third parties have access to your Personal Data only to perform these tasks on our behalf and are obligated not to disclose or use it for any other purpose.</p>
 
 
 
 
             <h2>Links to Other Sites</h2>
-            <p>Our Service may contain links to other sites that are not operated by us. If you click a third party link, you will be directed to that third party's site. We strongly advise you to review the Privacy Policy of every site you visit.</p>
+            <p>Our Service may contain links to other sites that are not operated by us. If you click a third party link, you will be directed to that third party&#39s site. We strongly advise you to review the Privacy Policy of every site you visit.</p>
             <p>We have no control over and assume no responsibility for the content, privacy policies or practices of any third party sites or services.</p>
 
 
-            <h2>Children's Privacy</h2>
-            <p>Our Service does not address anyone under the age of 18 ("Children").</p>
+            <h2>Children&#39s Privacy</h2>
+            <p>Our Service does not address anyone under the age of 18 (&#34Children&#34).</p>
             <p>We do not knowingly collect personally identifiable information from anyone under the age of 18. If you are a parent or guardian and you are aware that your Child has provided us with Personal Data, please contact us. If we become aware that we have collected Personal Data from children without verification of parental consent, we take steps to remove that information from our servers.</p>
 
 
             <h2>Changes to This Privacy Policy</h2>
             <p>We may update our Privacy Policy from time to time. We will notify you of any changes by posting the new Privacy Policy on this page.</p>
-            <p>We will let you know via email and/or a prominent notice on our Service, prior to the change becoming effective and update the "effective date" at the top of this Privacy Policy.</p>
+            <p>We will let you know via email and/or a prominent notice on our Service, prior to the change becoming effective and update the &#34effective date&#34 at the top of this Privacy Policy.</p>
             <p>You are advised to review this Privacy Policy periodically for any changes. Changes to this Privacy Policy are effective when they are posted on this page.</p>
 
 
