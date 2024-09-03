3fd7fd92b6f502f831f3e03c41225b1b28cf7e83
Nicholas Brown <nicholas.brown@jpl.nasa.gov>
2024-09-03 14:01:08 -0700

    Add new cards for core playtesting

--- config/roles.js
+++ config/roles.js
@@ -1,19 +1,26 @@
 const roles = {
     'core': {
         label: 'Core',
-        userEmails: ['nbrown4296@gmail.com']
+        userEmails: [
+            'nbrown4296@gmail.com',
+            'dallalanamackenzie@gmail.com',
+            'larson.n12@gmail.com',
+            'simon.girard@gmail.com'
+        ]
     },
-    'inner rim': {
-        label: 'Inner Rim',
-        userEmails: []
-    },
-    'mid rim': {
-        label: 'Mid Rim',
-        userEmails: []
-    },
-    'outer rim': {
-        label: 'Outer Rim',
-        userEmails: []
+    'playtester': {
+        label: 'Playtesters',
+        userEmails: [
+            'ifranklin@ntlworld.com',
+            'sebastianrauscher1987@googlemail.com',
+            'fritzofgalatia@gmail.com',
+            'samsimpsonbbf@gmail.com',
+            'thomsonjohn1988@gmail.com',
+            'seb.legare@gmail.com',
+            'mksmith7789@gmail.com',
+            'patrickryandoupe@gmail.com',
+            'andrewjgraber@gmail.com'
+        ]
     }
 };
 
--- src/common/CardButton.js
+++ src/common/CardButton.js
@@ -1,9 +1,11 @@
 import {
     Box,
-    Typography, 
+    Typography,
+    Chip,
     Card,
     CardActionArea,
-    CardMedia
+    CardMedia,
+    CardActions
 } from '@mui/material';
 import cards from 'config/cards.js';
 import versions from 'config/versions';
@@ -12,6 +14,33 @@ import versions from 'config/versions';
 function CardButton({ id, version, isDisabled, onClick, cardStyles = {} }) {
     if (id === true) return undefined;
     const card = cards.cardsById[id];
+    const { footnoteChanges } = versions[version];
+    const footnoteActions = []; // not actually "actions", more just notes
+    if (id in footnoteChanges && footnoteChanges[id].length > 0) {
+        footnoteChanges[id].forEach((footnote, i) => {
+            footnoteActions.push(
+                <Chip
+                    size="small"
+                    key={`footnote-${i}`}
+                    label={footnote}
+                />
+            );
+        });
+    }
+    if (id in versions[version].pointDeltas) {
+        const pointDelta = versions[version].pointDeltas[id];
+        footnoteActions.push(
+            <Chip
+                size="small"
+                key="pt-change-footnote"
+                label={pointDelta > 0 ? (
+                    `Points: ${card.points} -> ${card.points + pointDelta}`
+                ) : (
+                    `Points: ${card.points} -> ${card.points + pointDelta}`
+                )}
+            />
+        );
+    }
     return (
         <Card
             sx={{ marginRight: 1, marginBottom: 1 }}
@@ -46,6 +75,11 @@ function CardButton({ id, version, isDisabled, onClick, cardStyles = {} }) {
                     </Typography>
                 </Box>
             </CardActionArea>
+            {version > 0 ? (
+                <CardActions>
+                    {footnoteActions.length > 0 ? footnoteActions : <Chip size="small" label="Unchanged" />}
+                </CardActions>
+            ) : undefined}
         </Card>
     );
 }
