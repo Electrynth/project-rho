5db489ffcb8c098517ee6cad0f9f5dfd0e5e41ce
Nicholas Brown <nicholas.brown@jpl.nasa.gov>
2025-07-31 22:51:21 -0700

    New stuff

--- config/versions.js
+++ config/versions.js
@@ -10,7 +10,7 @@
 const versions = [
     {
         id: 0,
-        label: "Official Game",
+        label: "ARC01",
         description: "Current official ruleset for organized play.",
         roles: ["everyone"],
         pointDeltas: {},
@@ -131,36 +131,6 @@ const versions = [
                 "After an enemy ship resolves a command, you may discard a matching command token from this card. If you do, that enemy ship gains 1 matching raid token.",
                 "While a friendly squadron is attacking, if it is at distance 1-2 of an enemy ship with at least 1 raid token, it may reroll 1 die."
             ]
-        }
-    },
-    {
-        id: 3,
-        label: "ARC Open Beta Tournament",
-        description: "The primary goal for this tournament is to give the community a fun way to pass the time while we wall wait for the last AMG update. This tournament also will help ARC perform a few fact-finding missions on complicated topics.",
-        roles: ["everyone"],
-        enabledCards: ["tn"],
-        disabledCards: ["no"],
-        footnoteChanges: {},
-        textChanges: {
-            "il": ["Admiral Screed should now read: Once per activation, when a friendly ship is attacking, it may spend 1 die to change a die to a face with a [crit] icon. That die cannot be rerolled."],
-            "tn": ["Change faction to: Galactic Republic or Separatists only."],
-            "ld": ["Ordnance Experts should now read: While attacking, you may reroll any number of your black dice."],
-            "dg": ["A player may only score a maximum of 6 tokens more than their opponent (90pts)."],
-            "cp": ["A player may only score a maximum of 9 tokens more than their opponent (90pts)."],
-            "du": ["A player may only score a maximum of 6 tokens more than their opponent (90pts)."],
-            "dx": ["A player may only score a maximum of 6 tokens more than their opponent (90pts)."],
-            "dq": ["Each player may only choose a non-flotilla ship to be an objective ship."],
-            "cv": ["The second player may only choose non-flotilla ships to be an objective ship."],
-            "co": ["The first player may only choose one of their non-flotilla ships to be an objective ship. The second player may only choose one of their non-flotilla ships to be an objective ship."]
-        },
-        pointDeltas: {
-            "qf": 4,
-            "gc": 4,
-            "mt": -4,
-            "mx": -2,
-            "sw": 2,
-            "ng": 2,
-            "lq": 2
         }
     }
 ];
