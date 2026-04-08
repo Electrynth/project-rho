f14ad86a7a4f75402ca9fddca5076dc99e17fdca
Nick <37748722+Electrynth@users.noreply.github.com>
2026-04-08 12:36:34 -0700

    Fix syntax error in versions.js

--- config/versions.js
+++ config/versions.js
@@ -54,7 +54,7 @@ const versions = [
             "iz": -2,
             "kl": -3,
             "kn": -2,
-            "ji": 1,,
+            "ji": 1,
             "en": -1,
             "fn": -1,
             "fu": -1,
